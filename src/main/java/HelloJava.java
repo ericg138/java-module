a3357b503c6c54a18011d0d49d28d7daf77eabff
Eric Giffon <eric.giffon@sonarsource.com>
2024-01-16 16:25:55 +0100

    test

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -9,6 +9,10 @@ public class HelloJava {
 
   public void hello() {
     System.out.println("hello" + " java");
+
+    String unused1 = "unused1";
+    String unused2 = "unused2";
+    String unused3 = "unused3";
   }
 
   private static ThreadLocalRandom random() {
